3bc5195f696a641c8b782fbd499402ee6acfac3c
Oleksandr <a.chornenkyi.jsn@gmail.com>
2025-10-19 14:37:16 +0300

    feat: pwa settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,73 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.ico",
+        "apple-touch-icon.png",
+        "favicon-96x96.png",
+        "assets/*.png",
+        "assets/*.svg",
+        "assets/*.css",
+      ],
+      manifest: {
+        name: "Construction tasks",
+        short_name: "Tasks",
+        description: "Construction Tasks Management Application",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "favicon-96x96.png",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+        theme_color: "#155dfc",
+        background_color: "#fbf9fa",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
 });
